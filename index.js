cf510f7fccf90e509b000c8d6c59ab51dd6391b4
Saurabh Shambharkar <sshambharkar091@gmail.com>
2024-10-07 12:42:53 +0530

    index file added

---
+++ index.js
@@ -0,0 +1,15 @@
+const numbers = [
+    3, 7, 2, 15, 9, 20, 5, 14, 12, 1, 8, 11, 6, 19, 4, 10, 17, 13, 16, 18,
+  ];
+
+ function evenodd(numbers){
+    for(let i=0; i<numbers.length; i++){
+        if(numbers[i]%2 == 0)
+        {
+            console.log(numbers[i], "is even number");
+        }
+        else
+        console.log(numbers[i], "is odd number");
+    }
+ }
+ evenodd(numbers);
